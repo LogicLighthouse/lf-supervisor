ee7a3858910453fb0f9fa9ab884c111c427b4f16
fqaisrani <fqaisrani@yahoo.com>
2024-03-25 16:25:46 -0400

    DI

--- Backend/SMNS/Program.cs
+++ Backend/SMNS/Program.cs
@@ -1,8 +1,12 @@
+using SMNS.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+// Register the SupervisorService with its interface for DI
+builder.Services.AddHttpClient<ISupervisorService, SupervisorService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
