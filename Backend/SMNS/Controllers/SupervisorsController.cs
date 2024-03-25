7185ca8981bd32ecf4d8a97ae7c4a7f7c134230a
fqaisrani <fqaisrani@yahoo.com>
2024-03-25 16:34:06 -0400

    Controller + json fix

---
+++ Backend/SMNS/Controllers/SupervisorsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SMNS.Services;
+using System.Threading.Tasks;
+
+namespace SMNS.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SupervisorsController : ControllerBase
+    {
+        private readonly ISupervisorService _supervisorService;
+
+        public SupervisorsController(ISupervisorService supervisorService)
+        {
+            _supervisorService = supervisorService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var supervisors = await _supervisorService.GetSupervisorsAsync();
+            return Ok(supervisors);
+        }
+    }
+}
--- Backend/SMNS/Models/Supervisor.cs
+++ Backend/SMNS/Models/Supervisor.cs
@@ -1,12 +1,25 @@
-﻿namespace SMNS.Models
+﻿using System.Text.Json.Serialization;
+
+namespace SMNS.Models
 {
     public class Supervisor
     {
+        [JsonPropertyName("id")]
         public string Id { get; set; }
+
+        [JsonPropertyName("phone")]
         public string Phone { get; set; }
+
+        [JsonPropertyName("jurisdiction")]
         public string Jurisdiction { get; set; }
+
+        [JsonPropertyName("identificationNumber")]
         public string IdentificationNumber { get; set; }
+
+        [JsonPropertyName("firstName")]
         public string FirstName { get; set; }
+
+        [JsonPropertyName("lastName")]
         public string LastName { get; set; }
     }
 }
